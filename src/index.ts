3816da952d5347581b5b9e4cb6e782efe7aec36d
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-01-12 08:57:13 +0000

    M package.json  M src/index.ts  M tsconfig.json  M yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import createTransformer from 'tailwind-group-variant'
-const tailwindGroup = createTransformer({ separatorChar: ' ' })
+import cn from 'get-classnames'
+
+const tailwindGroup = (code: string) => {
+  const variantGroupsRegex = /([a-z\-0-9:]+:)\((.*?)\)/g
+  const variantGroupMatches = [...code.matchAll(variantGroupsRegex)]
+
+  variantGroupMatches.forEach(([matchStr, variants, classes]) => {
+    const parsedClasses = cn.prefix(variants, classes)
+    code = code.replace(matchStr, parsedClasses)
+  })
+
+  return code
+}
+
 export default tailwindGroup
