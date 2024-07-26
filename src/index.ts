c8805a578f5367bf325fa41d415d410dc67ebae3
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-07-26 10:13:41 +0600

    updated to string literal

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
-const tailwindVariantRegex = /(([a-z\-0-9]+|\[.*\]))\:\(.*?\)/gim
+const tailwindVariantRegex = /(([a-z\-0-9]+|\[.*\]))\:\{(.*?)\}/gim
 
 function prefixed(prefix: string, ...args: string[]) {
   return args.map((a) => prefix + a).join(' ')
 }
 
 function extractVariant(code: string) {
-  const { prefix, value } = code.match(/(?<prefix>.*)\((?<value>.*)\)/)!.groups!
-  const classes = prefixed(prefix, ...value.split(/ +/))
-  return classes
+  const { prefix, value } = code.match(/(?<prefix>.*)\{(?<value>.*)\}/)!.groups!
+  return prefixed(prefix, ...value.split(/ +/))
 }
 
 const transform = (code: string) => {
   return code.replace(tailwindVariantRegex, extractVariant)
 }
 
-function tw(...args: string[]) {
-  return args.map(transform).join(' ')
+function tw(strings: TemplateStringsArray, ...args: NestedArray) {
+  const flattedArgs = args.map((arg) =>
+    Array.isArray(arg) ? (arg as any[]).flat(Infinity).join(' ') : arg
+  )
+
+  const combined = strings.reduce(
+    (acc, str, i) => acc + str + (flattedArgs[i] || ''),
+    ''
+  )
+
+  return transform(combined)
 }
 
 export default tw
 export { transform, tw }
+type NestedArray = (string | number | NestedArray)[]
