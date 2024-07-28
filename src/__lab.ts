af988b7a9a45bfc8a0dcfec8a5d60e244571d142
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-07-29 01:51:26 +0600

    bench

--- src/__lab.ts
+++ src/__lab.ts
@@ -1,8 +1,22 @@
 console.clear()
 import tw from '.'
 
-console.log(
+function test(fb: Function) {
+  console.time('tw')
+  for (let i = 0; i < 1_000_000; i++) fb()
+  console.timeEnd('tw')
+}
+
+test(() => {
+  tw("")
+})
+
+test(() => {
   tw(
     "group/data-[custom]:md:focus-[visible]:hover:lg:after:{content-[''] w-[20px] h-[20px]}"
   )
-)
+})
+
+test(() => {
+  tw("after:{content-[''] w-[20px] h-[20px]}")
+})
